df7cc75d1e161239f21ca5a0a2fed31726036256
Mintoska <mintas@inbox.lt>
2023-04-14 14:49:18 +0300

    click&css

--- scripts/galery.js
+++ scripts/galery.js
@@ -1,5 +1,8 @@
 
+
 const modal = document.getElementById('modalai');
+
+import Modal from '../scripts/Modal.js';
 
 fetch('../scripts/data.json')
   .then(response => response.json())
@@ -24,7 +27,72 @@ fetch('../scripts/data.json')
       const vFotke = document.createElement('img');
       vFotke.setAttribute('src', fotke);
 
-      newMod.append(vardas, aprasy, vFotke, text);
+      newMod.append(vFotke);
       modal.appendChild(newMod);
+
+      vFotke.addEventListener('click', () => {
+        newMod.removeChild(vFotke);
+
+        const visaInfo = document.createElement('div');
+        visaInfo.classList.add('visaInfo');
+        visaInfo.append(vardas);
+        visaInfo.append(text);
+        visaInfo.append(aprasy);
+        newMod.append(visaInfo);
+
+        const closeModal = () => {
+          newMod.removeChild(visaInfo);
+          newMod.append(vFotke);
+          document.removeEventListener('spaustEsc', onEscKeyDown);
+        };
+        const onEscKeyDown = event => {
+          if (event.key === 'Escape') {
+            closeModal();
+          }};
+        document.addEventListener('spaustEsc', onEscKeyDown);
+        visaInfo.addEventListener('click', closeModal);
+      });
     });
   });
+
+  //     vFotke.addEventListener('click', () => {
+  //       const newDiv = document.createElement('div');
+  //       newDiv.classList.add('modalai');
+  //       newDiv.appendChild(vardas);
+  //       newDiv.appendChild(text);
+  //       newDiv.appendChild(aprasy);
+
+  //       newMod.replaceChild(newDiv, vFotke);
+  //     });
+
+  //     newMod.append(vFotke);
+  //     modal.appendChild(newMod);
+  //   });
+  // });
+
+
+      // paspaudimas ant fotkės
+
+      // newMod.append(vardas);
+      // newMod.append(text);
+      // newMod.append(aprasy);
+      // modal.appendChild(newMod);
+
+      // vFotke.addEventListener('click', () => {
+      //   newMod.removeChild(vFotke);
+      // });
+
+      // vFotke.addEventListener('click', () => {
+      //   const newDiv = document.createElement('div');
+      //   const newAprasy = document.createElement('p');
+      //   const newTekstas = document.createTextNode(apie);
+      //   newAprasy.appendChild(newTekstas);
+      //   const newTur = document.createElement('p');
+      //   const newVa = document.createTextNode(apras);
+      //   newTur.appendChild(newVa);
+      //   newDiv.append(newAprasy);
+      //   newDiv.append(newTur);
+      //   newMod.append(newDiv);
+      // });
+  //   });
+  // });
